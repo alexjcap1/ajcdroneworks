026c6eedc62c15da5db96ba1027448adbdc246d5
Alexander Capicchioni <alexjcap1@gmail.com>
2025-06-03 22:30:41 -0400

    Add dynamic sitemap at /api/sitemap

---
+++ app/api/sitemap/route.js
@@ -0,0 +1,32 @@
+export async function GET() {
+  const baseUrl = "https://ajcdroneworks.com";
+
+  const staticPaths = [
+    "",             // Home
+    "portfolio",
+    "contact",
+    "services",
+  ];
+
+  const urls = staticPaths
+    .map((path) => {
+      return `
+        <url>
+          <loc>${baseUrl}/${path}</loc>
+          <priority>${path === "" ? "1.0" : "0.8"}</priority>
+        </url>
+      `;
+    })
+    .join("");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${urls}
+    </urlset>`;
+
+  return new Response(sitemap, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
